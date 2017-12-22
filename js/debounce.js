110bbdcc79c5af4c52e173fe0c8eec1ca6e29845
marikap <m8771@mail.ru>
2017-12-22 12:44:17 +0300

    module7-task1

---
+++ js/debounce.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  var debounceInterval = 500;
+
+  var lastTimeout;
+  window.debounce = function (fun) {
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(fun, debounceInterval);
+  };
+})();
--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,10 @@
 
 (function () {
 
+  var filters = document.querySelector('.filters');
+  var unsortedData;
+  var sortedData;
+  
   var loadObjects = function (block, objects) {
     var fragment = document.createDocumentFragment();
     for (var i = objects.length - 1; i >= 0; i--) {
@@ -26,5 +30,47 @@
       },
       window.util.displayError
   );
+  var currentFilter = 'recommend';
+  var applyFilter = function (evt) {
+    if (evt.target.type !== 'radio') {
+      return;
+    }
+    var filter = evt.target.value;
+    if (filter === currentFilter && filter !== 'random') {
+      return;
+    }
+    switch (filter) {
+      // Популярные фотографии
+      case 'popular':
+        sortedData = unsortedData.slice(0).sort(function (first, second) {
+          return second.likes - first.likes;
+        });
+        break;
+      // Обсуждаемые фотографии
+      case 'discussed':
+        sortedData = unsortedData.slice(0).sort(function (first, second) {
+          return second.comments.length - first.comments.length;
+        });
+        break;
+      // Случайные
+      case 'random':
+        var copyData = unsortedData.slice(0);
+        sortedData = [];
+        while (copyData.length > 0) {
+          var indexElement = Math.floor(Math.random() * copyData.length);
+          sortedData.push(copyData.splice(indexElement, 1)[0]);
+        }
+        break;
+      // Рекомендуемые
+      case 'recommend':
+      default:
+        sortedData = unsortedData;
+    }
+    window.debounce(createGallery);
+    currentFilter = filter;
+  };
 
+  filters.addEventListener('click', function (evt) {
+    applyFilter(evt);
+  });
 })();
